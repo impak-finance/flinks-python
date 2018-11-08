3b756c0ad2ec7f95a213c9df7ed05cf7bead00ea
Morgan Aubert <morgan.aubert@impakfinance.com>
2018-11-08 10:55:39 -0500

    Added new API endpoints

--- flinks/entities/banking_services.py
+++ flinks/entities/banking_services.py
@@ -8,6 +8,8 @@
 
 """
 
+import datetime as dt
+
 from ..baseapi import BaseApi
 
 
@@ -90,3 +92,77 @@ class BankingServices(BaseApi):
         return self._client._call(
             'POST', self._build_path('GetAccountsSummary'), data={'RequestId': request_id, },
         )
+
+    def get_accounts_detail(
+        self, request_id, with_account_identity=False, with_transactions=False,
+        days_of_transactions=None, date_from=None, date_to=None, refresh_delta=None,
+        accounts_filter=None,
+    ):
+        """ Retrieves complete details about a specific user.
+
+        :param request_id: valid request ID
+        :param with_account_identity:
+            whether to include information about the account in the response
+        :param with_transactions: whether to include account transactions in the response
+        :param days_of_transactions:
+            identifier of the number of days of transactions needed (either 'Days90' or 'Days365')
+        :param date_from: start date to use to retrieve transactions
+        :param date_to: end date to use to retrieve transactions
+        :param refresh_delta:
+            list of dictionaries containing an 'AccountId' and 'TransactionId'; they will be used in
+            order to return transactions that occured after the specified transactions only
+        :param accounts_filter: list of user account IDs to target specificaly
+        :type request_id: str
+        :type with_account_identity: bool
+        :type with_transactions: bool
+        :type days_of_transactions: str
+        :type date_from: datetime.datetime or datetime.date or str
+        :type date_to: datetime.datetime or datetime.date or str
+        :type refresh_delta: list
+        :type accounts_filter: list
+        :return: dictionary containing the complete details of the user
+        :rtype: dictionary
+
+        """
+        def _prepare_date(date):
+            date = date.date() if isinstance(date, dt.datetime) else date
+            return date.isoformat() if isinstance(date, dt.date) else date
+
+        data = {
+            'RequestId': request_id,
+            'WithAccountIdentity': with_account_identity,
+            'WithTransactions': with_transactions,
+        }
+        if days_of_transactions:
+            data['DaysOfTransactions'] = days_of_transactions
+        if date_from:
+            data['DateFrom'] = _prepare_date(date_from)
+        if date_to:
+            data['DateTo'] = _prepare_date(date_to)
+        if refresh_delta:
+            data['RefreshDelta'] = refresh_delta
+        if accounts_filter:
+            data['AccountsFilter'] = accounts_filter
+        return self._client._call('POST', self._build_path('GetAccountsDetail'), data=data)
+
+    def get_accounts_detail_async(self, request_id):
+        """ Retrieves complete details about a specific user (async mode).
+
+        :param request_id: valid request ID
+        :type request_id: str
+        :return: dictionary containing the complete details of the user
+        :rtype: dictionary
+
+        """
+        return self._client._call('GET', self._build_path('GetAccountsDetailAsync/' + request_id))
+
+    def delete_card(self, login_id):
+        """ Deletes all traces of information about a card on Flinks side.
+
+        :param login_id: valid login ID
+        :type login_id: str
+        :return: dictionary containing the complete details of the user
+        :rtype: dictionary
+
+        """
+        return self._client._call('DELETE', self._build_path('DeleteCard/' + login_id))
